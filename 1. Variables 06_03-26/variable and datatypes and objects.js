f42bb6fa9568d932dd2c50c1665f23d2de7ae6f7
abhimithun4u <abhimithun4u@gmail.com>
2026-03-07 12:36:53 +0530

    Second comit variables, operators and conditional statements

---
+++ 1. Variables 06_03-26/variable and datatypes and objects.js
@@ -0,0 +1,44 @@
+//Declaration of normal variables
+let fname ="Abhishek";
+let age = 44;
+let city ="Kolkata";
+console.log("Hello " +fname);
+console.log("Your Age is "+age);
+console.log("You live in "+city);
+console.log(typeof(fname));
+console.log(typeof(age));
+// Boolean Variable
+let isLearning=true;
+console.log("Learning JavaScript: ",isLearning);
+//null variable declaration and showing its type
+let x=null;
+console.log(x);
+console.log(typeof(x))
+// undefined variable declaration and showing its type
+let y;
+console.log(typeof(y));
+
+//practice set for availability of a product
+let product ="Phillips Air Frier";
+let price = 5500;
+let isAvailable = true;
+console.log(product +" is Available : " +isAvailable + " and its price is Rs "+price );
+
+// practice set to update a variable
+
+let score =50;
+console.log(score);
+score =85;
+console.log("The Updated Score is now "+score);
+
+// Practice set to define an object and print the object, find type of the object and print its individual elements
+const employee ={
+    fname:"Abhishek",
+    lname:"Mukherjee",
+    empid:"DS005",
+    age:43,
+    isWorking:true,
+    isLearningJS:true,
+};
+console.log(employee);
+console.log("Welcome "+employee["fname"]+" "+employee["lname"]+" ! Your Employee ID is "+employee["empid"]+" and your age is "+employee["age"]+ " and you are working "+employee["isWorking"]+ " and your learning status is "+employee["isLearningJS"]);
--- first.js
+++
@@ -1,4 +0,0 @@
-let name ="Abhishek";
-let age = 44;
-console.log("Hello" +name);
-console.log(age);
