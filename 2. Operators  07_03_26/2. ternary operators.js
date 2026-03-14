de9139a2a7a1d28551cfeb619d2fc7826dd51c06
abhimithun4u <abhimithun4u@gmail.com>
2026-03-14 21:39:56 +0530

    practice ternary operator

--- 2. Operators  07_03_26/2. ternary operators.js
+++ 2. Operators  07_03_26/2. ternary operators.js
@@ -3,7 +3,14 @@
 let result = age>=18 ? "Adult" : "Minor";
 console.log(result); */
 
+
+
 // Second ternary operator practice
-let marks=30;
+/* let marks=30;
 let result = marks>=35 ? "Pass": "Fail";
-console.log(result)
+console.log(result) */
+
+// Program to find Odd and Even Number using ternary operators
+let num =4;
+let result =num%2==0 ? "Even" : "Odd";
+console.log(result);
