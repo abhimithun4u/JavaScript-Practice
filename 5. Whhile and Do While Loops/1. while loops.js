3592590cff7b2934413da611f8dd703b6554a53b
abhimithun4u <abhimithun4u@gmail.com>
2026-03-15 15:45:39 +0530

    while and do while loops parctice 15_03_2026

---
+++ 5. Whhile and Do While Loops/1. while loops.js
@@ -0,0 +1,50 @@
+// program to understand basich while loop structure
+
+/* let i=1;
+let n=10;
+while(i<=n)
+{
+    console.log(i);
+    i++;
+} */
+
+//Program to print odd number from 1 to 10
+/* let i=0;
+let n=10;
+while(i<10){
+    if(i%2!==0)
+        console.log(i);
+    i++
+} */
+
+//Print numbers 10 to 1 (reverse order).
+/* let i=10;
+while(i>0)
+{
+    console.log(i);
+    i--;
+} */
+
+
+//Find sum of numbers from 1 to 10 using while loop
+/* let i=1;
+let sum=0;
+while(i<=10)
+{
+    sum+=i;
+    i++;
+}
+console.log(sum); */
+
+
+//Print multiplication table of 3 using while loops
+let i=1;
+let n=3;
+console.log("Multiplication Table of "+ n);
+while(i<=10)
+{
+    let mt=n*i;
+    console.log(n+" * "+i+" =" +mt );
+    i++;
+}
+ 
---
+++ 5. Whhile and Do While Loops/2. do while loops.js
@@ -0,0 +1,71 @@
+// Program using do while loops to print from 1 to 10
+/* let i=1;
+do{
+    console.log(i);
+    i++;
+}
+while(i<=10); */
+
+
+// Program using do while loops to find and print even numbers from 1 to 10
+/* let i=1;
+let num =10;
+do{
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+    i++;
+}
+while(i<=num); */
+
+
+
+
+/* Print pattern using do while
+
+*****
+****
+***
+**
+*
+
+*/
+/* let n=5;
+do{
+let line="";
+    for(let j=1;j<=n;j++)
+    {
+        line =line+"*";
+    }
+     console.log(line);
+    n--;
+}
+while(n>=1);
+ */
+
+
+
+/* Print Pattern using do while loops
+*
+**
+***
+****
+*****
+
+*/
+let n=5;
+let i=1;
+do{
+    let line="";
+    for(let j=1;j<=i;j++)
+    {
+        line =line+"*";
+        
+    }
+    i++;
+     console.log(line);
+   
+}
+while(i<=n);
+    
