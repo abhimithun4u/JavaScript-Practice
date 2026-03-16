fc0c6d18e691741f792fd243e8e078abf72653ef
abhimithun4u <abhimithun4u@gmail.com>
2026-03-16 19:29:24 +0530

    arrow functions learning 16_03-26

---
+++ 6. Functions 15-03-26/2. arrow functions.js
@@ -0,0 +1,18 @@
+// Single parameter arrow function
+//let name="Abhishek Mukherjee";
+/* const greet = (name) => {
+  return "Hello " + name;
+};
+console.log(greet("Abhishek")); */
+/* const greet = name => "Hello " + name;
+console.log(greet(name)); */
+
+/* const square =side => side*side;
+console.log(square(5)); */
+// Defining arrow functions with multiple parameters
+/* const area =(length,width) => length*width;
+console.log(area(5,4)); */
+
+// Arrow function to calculate simple interest
+const si =(p,r,t) => (p*r*t)/100;
+console.log(si(10000,6.5,3));
