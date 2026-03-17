a7870c134b8b9001741a7bf657b8ca0902aba786
abhimithun4u <abhimithun4u@gmail.com>
2026-03-17 09:03:01 +0530

    arrow functions

--- 6. Functions 15-03-26/2. arrow functions.js
+++ 6. Functions 15-03-26/2. arrow functions.js
@@ -14,5 +14,22 @@ console.log(square(5)); */
 console.log(area(5,4)); */
 
 // Arrow function to calculate simple interest
-const si =(p,r,t) => (p*r*t)/100;
-console.log(si(10000,6.5,3));
+/* const si =(p,r,t) => (p*r*t)/100;
+console.log(si(10000,6.5,3)); */
+
+// program two swap two numbers without third variable 5,7 
+/* const swap =(a,b) => {
+    a=a-b;
+    b=a+b;
+    a=b-a;
+    return [a,b];
+}
+console.log("The Reversed value of A and B is"+swap(6,7)); */
+
+// Program using arrow function to convert Celcius to Fahrenheit F = (C × 9/5) + 32
+const tempConverter =(c)=>{
+    let f = (c * 9/5) + 32;
+    return f;
+}
+console.log("The Converted Temperature is:- "+tempConverter(45));
+
