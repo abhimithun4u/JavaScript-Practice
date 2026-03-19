c9fb6b9a55b6bfeb2f8967dea9e00c67bb83f0df
abhimithun4u <abhimithun4u@gmail.com>
2026-03-19 20:03:34 +0530

    learning arrays

---
+++ 8. Arrays 18_03_26/2. array_methods.js
@@ -0,0 +1,47 @@
+// to string function converts and array to a string -> arr.toString() // Returns string
+/* let arr=[23,34,43,67,12,76,89,15];
+let str=arr.toString();
+console.log(str);
+
+
+// join function allows array elements to be joined using provided delimeter, it returns a string
+let jn =arr.join(" + ");
+console.log(jn);
+
+// function to remove last element of an array array pop
+arr.pop();
+console.log(arr);
+
+// Function to add an array element at the last of the array -> push
+arr.push(49);
+console.log(arr); */
+
+// Function to remove the first element from the array -> shift()
+/* let array = [34,79,65,23,50,122,76];
+array.shift();
+console.log(array);
+
+// Function to add the first element from the array -> unshift()
+array.unshift(126);
+console.log(array); */
+
+// Function to check whether the element is present within an array includes() → check existence
+let arr = ["Abhishek","Saanvi","Nikhil","Rohan","Riya"];
+if(arr.includes("Riya"))
+{
+    console.log("Riya is available in the Array");
+}
+else console.log("Riya is Not Available")
+
+// Function to delete an array element -> delete function deltes the element without changing the array size
+
+
+// Fuction to concat two arrays
+
+// function to sort -> sort function sort the data alphabetically, 
+// if you want to sort in asc or desc you need to use the compaire function
+
+
+// Splice and slice function
+
+// Reverse function
