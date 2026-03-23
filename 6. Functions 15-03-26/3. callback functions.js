edf2ef1cf772098f92dad269703370eea566f584
abhimithun4u <abhimithun4u@gmail.com>
2026-03-23 09:37:31 +0530

    callback

--- 6. Functions 15-03-26/3. callback functions.js
+++ 6. Functions 15-03-26/3. callback functions.js
@@ -35,5 +35,73 @@ function displayResult(result){
 
 } */
 
+/* // Calback function to perform basic calculator function
+calculate(sum,6,7);
+calculate(sub,9,7);
+calculate(mul,3,4);
+calculate(div,6,2);
+function calculate(callback,a,b){
+console.log(callback.name + " of two numbers is: " + callback(a, b));
+};
+function sum(num1,num2){
+    return num1+num2;
+}
 
+function sub(num1,num2){
+    return num1-num2;
+}
 
+function mul(num1,num2){
+    return num1*num2;
+}
+
+function div(num1,num2){
+    return num1/num2;
+} */
+
+/* //Program using callback function display various login status
+
+function executeAction(callback){
+    console.log("Executing "+callback.name);
+    callback();
+}
+function login(){
+   console.log("User Logged In");
+    }
+function fetchData(){
+   console.log("Data fetched");
+    }
+function logout(){
+    console.log("User Logged Out");
+   }
+
+executeAction(login);
+executeAction(fetchData);
+executeAction(logout); */
+
+//Program using callback to execute ecommerce flow
+function createUser(name,email,callback){
+    console.log(`User Created ! ${name},${email}`)
+    callback(name,email);
+    }
+
+function placeOrder(user,product,price,callback){
+    console.log(`${user} has ordered ${product} for ${price}`)
+    callback(user,product,price);
+}
+
+function makePayment(user,amount,callback){
+    console.log(`Payement for ${amount} has been made succssfully by ${user}`)
+    callback(user,amount);
+}
+function sendConfirmation(user,callback){
+    console.log(`Confirmation Message sent to ${user}`);
+    }
+
+createUser("Abhishek", "abhi@gmail.com", (user) => {
+  placeOrder(user, "Laptop", 50000, (user, price) => {
+    makePayment(user, price, (user) => {
+      sendConfirmation(user);
+    });
+  });
+});
